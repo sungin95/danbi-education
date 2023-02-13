b3f0d910a1d6e973067127af0337a9bcdbcdab6e
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-13 17:42:15 +0900

    feat : soft deletes 추가 - routine에게 soft delete 패턴 추가 - today_todos 부분 수정 필요

--- routine/models.py
+++ routine/models.py
@@ -7,7 +7,29 @@ category_CHOICES = (
 )
 
 
-class Routine(models.Model):
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    class Meta:
+        abstract = True
+
+
+class Routine(SoftDeleteModel):
     routine_id = models.AutoField(primary_key=True)
     account_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
@@ -15,8 +37,6 @@ class Routine(models.Model):
     goal = models.CharField(max_length=150)
     # 알람 on off
     is_alarm = models.BooleanField(default=True)
-    # 삭제를 할려면 True로
-    is_deleted = models.DateTimeField(null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     # JSON으로 받으면 문자열로 받아야 하니까
