9344ba1e53695593382154b639be3aa4baab2f24
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-14 17:42:46 +0900

    feat : 로그인 실패시 메시지 수정 - exceptions 상황 발생시 처리하는 로직 추가 - 아직 이해가 잘 안되서 추가 공부 필요

--- config/settings.py
+++ config/settings.py
@@ -153,10 +153,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "core.exceptions.core_exception_handler",
+    "NON_FIELD_ERRORS_KEY": "error",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        # "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
 }
 
 AUTH_USER_MODEL = "accounts.User"
---
+++ core/exceptions.py
@@ -0,0 +1,33 @@
+from rest_framework.views import exception_handler
+
+
+def core_exception_handler(exc, context):
+    # 1.
+    response = exception_handler(exc, context)
+
+    # 2.
+    handlers = {"ValidationError": _handle_generic_error}
+
+    # 3.
+    exception_class = exc.__class__.__name__
+
+    # 4.
+    if exception_class in handlers:
+        return handlers[exception_class](exc, context, response)
+    # 5.
+    return response
+
+
+# 6.
+def _handle_generic_error(exc, context, response):
+    response.data = {"errors": response.data}
+
+    return response
+
+
+# 1. 추가 공부 필요....
+# 2.
+# 3.
+# 4.
+# 5.
+# 6.
