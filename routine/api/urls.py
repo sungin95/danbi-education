b252d5cd6dd1410409be810d5712ac9067f7390c
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-02-15 17:43:23 +0900

    style : 필요 없는 부분 삭제

--- routine/api/urls.py
+++ routine/api/urls.py
@@ -4,9 +4,4 @@ from . import views
 urlpatterns = [
     path("routine/", views.CreateRoutineAPIView.as_view()),
     path("routine_detail/<int:pk>/", views.RoutineDetailAPIView.as_view()),
-    # path("create/", views.createRoutine),
-    # path("today_todos/", views.today_todos),
-    # path("modify/<int:pk>/", views.modify),
-    # path("delete/<int:pk>/", views.delete),
-    # path("restore/<int:pk>/", views.restore),
 ]
